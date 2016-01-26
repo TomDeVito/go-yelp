b5984f568092c5cf6b578552c721bd8e5fe8870d
Tom DeVito <tom.devito22@gmail.com>
2016-01-26 15:56:13 -0500

    Update phone_options.go

--- yelp/phone_options.go
+++ yelp/phone_options.go
@@ -6,9 +6,9 @@ import (
 
 // PhoneOptions provide the ability to search for a business by phone number.
 type PhoneOptions struct {
-	phone    string // Parameter that specifies the business phone number to search for. Outside of the US and Canada, include the international dialing code (e.g. +442079460000) or use the 'cc' parameter
-	cc       string // ISO 3166-1 alpha-2 country code. Default country to use when parsing the phone number. United States = US, Canada = CA, United Kingdom = GB (not UK).
-	category string // Category to filter search results with. See the list of supported categories.
+	Phone    string // Parameter that specifies the business phone number to search for. Outside of the US and Canada, include the international dialing code (e.g. +442079460000) or use the 'cc' parameter
+	CC       string // ISO 3166-1 alpha-2 country code. Default country to use when parsing the phone number. United States = US, Canada = CA, United Kingdom = GB (not UK).
+	Category string // Category to filter search results with. See the list of supported categories.
 }
 
 // getParameters will reflect over the values of the given
